6f085029a16a79ca356a8b35b96d39ae4b13ce57
Alina Sidarava <alinasidorova2812@gmail.com>
2024-04-25 21:54:06 +0200

    feat(scylladb-bookshop): add application core

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React from 'react';
+
+function App() {
+    console.log('');
+
+    return (
+        <div className="App">
+            App
+        </div>
+    );
+}
+
+export default App;
